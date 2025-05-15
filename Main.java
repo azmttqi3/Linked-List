48d22aed2db8dfc95246ae8dcdbba66a9e500bcd
azmttqi3 <azmiittaqi03@gmail.com>
2025-05-15 09:00:48 +0700

    perbaikan

--- Main.java
+++ Main.java
@@ -2,38 +2,38 @@ public class Main {
     public static void main(String[] args){
         LinkedList list = new LinkedList();
 
-        //insert element
+        // Insert element
         list.insertAtEnd("Budi");
         list.insertAtBeginning("Andi");
         list.insertAtEnd("Citra");
         list.insertAtEnd("Dewi");
 
-        //Print the list
+        // Print the list
         System.out.println("Linked List after insertions:");
-        list.println(); // Expected: Andi -> Budi -> Citra -> Dewi -> null
+        list.printList(); // Expected: Andi -> Budi -> Citra -> Dewi -> null
 
-        //Insert after a spesific node
-        Node NodeBudi = list.getNode("Budi");
+        // Insert after a spesific node
+        Node nodeBudi = list.getNode("Budi");
         if (nodeBudi != null) {
             list.insertAfter(nodeBudi, "Eka");
         }
 
-        //  Print the list after insertAfter
+        // Print the list after insertAfter
         System.out.println("\nLinked List after inserting Eka after Budi:");
-        list.println() // Expected: Andi -> Budi -> Eka -> Citra -> Dewi -> null
+        list.printList(); // Expected: Andi -> Budi -> Eka -> Citra -> Dewi -> null
 
-        //Delete a node
+        // Delete a node
         list.deleteNode("Citra");
 
-        //Print the list after detetion
+        // Print the list after detetion
         System.out.println("\nLinked List after deleting Citra:");
-        list.println(); // Expected: Andi -> Budi -> Eka -> Dewi -> null
+        list.printList(); // Expected: Andi -> Budi -> Eka -> Dewi -> null
 
-        //Deleting a non-existent node
+        // Deleting a non-existent node
         list.deleteNode("Fajar");
 
         // Print the list after trying to delete non-existent node
         System.out.println("\nLinked List after trying yo deletie Fajar:");
-        list.println(); // Expected: Andi -> Budi -> Eka -> Dewi -> null
+        list.printList(); // Expected: Andi -> Budi -> Eka -> Dewi -> null
     }
 }        
